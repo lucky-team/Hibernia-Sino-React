684df2e3648bbedc474a3d026af16da058f5276f
YCaptain <506840597@qq.com>
2019-04-04 15:54:26 +0800

    build(env): cross-platformly set port, change server url

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://59.110.243.55/';
+export const baseUrl = 'http://59.110.243.55:3000/';
 // export const baseUrl = 'http://localhost:3000/';
 
 export const homePath = '/home';
